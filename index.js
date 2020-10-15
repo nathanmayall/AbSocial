0989d639fbab5974ecc18d07505d8cdbb62bf585
Nathan Mayall <nath@AbSocial.NathHouse.local>
2020-10-15 21:25:50 +0100

    modified paths for indes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { ApolloServer } = require('apollo-server'); 
 const mongoose = require('mongoose');
 
-const typeDefs = require('./AbSocial/graphql/typeDefs');
-const resolvers = require('./AbSocial/graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
 const server = new ApolloServer({
